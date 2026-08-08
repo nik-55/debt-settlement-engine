f7bc694307001d385902413985ba305bbb000e62
nik-55 <98151272+nik-55@users.noreply.github.com>
2026-08-08 23:21:28 +0530

    update engine to support staircase shape

--- feasibility/algo.py
+++ feasibility/algo.py
@@ -37,6 +37,41 @@ def next_month(d: datetime.date, is_last_day_celing, anchor_day):
     return datetime.date(year, month, min(anchor_day, last_day_of_month))
 
 
+def get_cadence_dates(offer: Offer, k_max: int):
+    cadence_dates = []
+    first_payment_date = offer.first_payment_date
+    iter_date = first_payment_date
+    i = 0
+
+    is_last_day_ceiling = (
+        get_last_day_in_month(first_payment_date.year, first_payment_date.month)
+        == first_payment_date.day
+    )
+
+    anchor_day = first_payment_date.day
+
+    while i < k_max:
+        i += 1
+        cadence_dates.append(iter_date)
+        iter_date = next_month(iter_date, is_last_day_ceiling, anchor_day)
+
+    return cadence_dates
+
+
+def calculate_even_pay_payments(total_offer: int, k: int):
+    base_amount = total_offer // k
+    remaining_cents = total_offer % k
+
+    creditor_payments = [base_amount] * k
+
+    for i in range(remaining_cents):
+        creditor_payments[k - 1 - i] += 1
+
+    print(f"Creditor payments: {creditor_payments}")
+
+    return creditor_payments
+
+
 def evaluate_offer_pipeline(
     client: Client, offer: Offer, rules: CreditorRules
 ) -> Result:
@@ -44,84 +79,74 @@ def evaluate_offer_pipeline(
     program_fee = round_half_up(offer.original_balance_cents * rules.program_fee_pct)
 
     k_max = min(rules.max_terms, rules.max_payments)
+    cadence_dates = get_cadence_dates(offer, k_max)
+
+    date_to_draft_map = {}
+
+    for draft in client.ledger:
+        date_to_draft_map[draft.date] = draft
+
+    movement_days = cadence_dates + list(date_to_draft_map.keys())
+    movement_days = sorted(movement_days)
+
+    print(f"Movement days: {movement_days}")
+
     if rules.even_pays:
-        base_amount = total_offer // k_max
-        remaining_cents = total_offer % k_max
+        creditor_payments = calculate_even_pay_payments(total_offer, k_max)
+        pay_shape_used = "even"
+    elif rules.is_ballooning_allowed:
+        pass
+    else:
+        pay_shape_used = "staircase"
+        if rules.max_segments >= 2:
+            creditor_payments = [rules.min_payment_cents] * rules.max_token_pays
 
-        creditor_payments = [base_amount] * k_max
+            remaining_k = k_max - rules.max_token_pays
+            amount_rem_to_creditor = total_offer - sum(creditor_payments)
 
-        for i in range(remaining_cents):
-            creditor_payments[k_max - 1 - i] += 1
+            remaining_payments = calculate_even_pay_payments(
+                amount_rem_to_creditor, remaining_k
+            )
+            creditor_payments += remaining_payments
 
-        print(f"Creditor payments: {creditor_payments}")
+    date_to_creditor_amount_map = {
+        cadence_dates[i]: payment for i, payment in enumerate(creditor_payments)
+    }
 
-        cadence_dates = []
-        first_payment_date = offer.first_payment_date
-        iter_date = first_payment_date
-        i = 0
+    current_escor_balance = client.current_balance_cents
+    program_fee_remaining = program_fee
 
-        is_last_day_ceiling = (
-            get_last_day_in_month(first_payment_date.year, first_payment_date.month)
-            == first_payment_date.day
-        )
+    rows = []
+    for date in movement_days:
+        if date in date_to_creditor_amount_map:
+            amount_to_creditor = date_to_creditor_amount_map[date]
+            bank_fee = 0
+            if amount_to_creditor > 0:
+                bank_fee = rules.bank_fee_cents
 
-        anchor_day = first_payment_date.day
+            remaining_amount = current_escor_balance - (amount_to_creditor + bank_fee)
 
-        while i < k_max:
-            i += 1
-            cadence_dates.append(iter_date)
-            iter_date = next_month(iter_date, is_last_day_ceiling, anchor_day)
+            p_fee = min(remaining_amount, program_fee_remaining)
+            program_fee_remaining -= p_fee
 
-        date_to_draft_map = {}
+            current_escor_balance -= amount_to_creditor + bank_fee + p_fee
 
-        for draft in client.ledger:
-            date_to_draft_map[draft.date] = draft
-
-        movement_days = cadence_dates + list(date_to_draft_map.keys())
-        movement_days = sorted(movement_days)
-
-        print(f"Movement days: {movement_days}")
-
-        date_to_creditor_amount_map = {
-            cadence_dates[i]: payment for i, payment in enumerate(creditor_payments)
-        }
-
-        current_escor_balance = client.current_balance_cents
-        program_fee_remaining = program_fee
-
-        rows = []
-        for date in movement_days:
-            if date in date_to_creditor_amount_map:
-                amount_to_creditor = date_to_creditor_amount_map[date]
-                bank_fee = 0
-                if amount_to_creditor > 0:
-                    bank_fee = rules.bank_fee_cents
-
-                remaining_amount = current_escor_balance - (
-                    amount_to_creditor + bank_fee
+            print(
+                f"Debited: Creditor {amount_to_creditor} Bank fee: {bank_fee} p_fee: {p_fee}"
+            )
+            rows.append(
+                ScheduleRow(
+                    date=date,
+                    creditor_payment_cents=amount_to_creditor,
+                    program_fee_cents=p_fee,
+                    bank_fee_cents=bank_fee,
+                    balance_cents=current_escor_balance,
                 )
+            )
+        elif date in date_to_draft_map:
+            draft = date_to_draft_map[date]
+            current_escor_balance += draft.amount_cents
+            print(f"Credited {draft.amount_cents}. Total: {current_escor_balance}")
 
-                p_fee = min(remaining_amount, program_fee_remaining)
-                program_fee_remaining -= p_fee
-
-                current_escor_balance -= amount_to_creditor + bank_fee + p_fee
-
-                print(
-                    f"Debited: Creditor {amount_to_creditor} Bank fee: {bank_fee} p_fee: {p_fee}"
-                )
-                rows.append(
-                    ScheduleRow(
-                        date=date,
-                        creditor_payment_cents=amount_to_creditor,
-                        program_fee_cents=p_fee,
-                        bank_fee_cents=bank_fee,
-                        balance_cents=current_escor_balance,
-                    )
-                )
-            elif date in date_to_draft_map:
-                draft = date_to_draft_map[date]
-                current_escor_balance += draft.amount_cents
-                print(f"Credited {draft.amount_cents}. Total: {current_escor_balance}")
-
-        result = Result(feasible=True, schedule=rows, pay_shape_used="even")
-        return result
+    result = Result(feasible=True, schedule=rows, pay_shape_used=pay_shape_used)
+    return result
